9db405eeafb2f6a5d93b1ed743a0fcce7b1fc33c
YevheniiaUhnich <140949326+YevheniiaUhnich@users.noreply.github.com>
2025-04-21 17:20:50 +0300

    up1

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -1,6 +1,6 @@
 import s from "./LoadMoreBtn.module.css";
 
-const LoadMoreBtn = ({ setPage, totalPage, page }) => {
+const LoadMoreBtn = () => {
   return <button className={s.loadMoreBtn}>Load more</button>;
 };
 
